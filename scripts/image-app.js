fe427916ee963cc42e82a92c27c653f7b1757f07
Itachi-MacOS <warrior.006@inbox.ru>
2023-07-30 03:06:52 +0600

    using workers to offload main thread

--- scripts/image-app.js
+++ scripts/image-app.js
@@ -1,5 +1,6 @@
 (function(){
   // http://stackoverflow.com/questions/10906734/how-to-upload-image-into-html5-canvas
+  const ImageManipulationWorker = new Worker('scripts/worker.js')
   var original;
   var imageLoader = document.querySelector('#imageLoader');
   imageLoader.addEventListener('change', handleImage, false);
@@ -40,23 +41,11 @@
 
     toggleButtonsAbledness();
 
-    // Hint! This is where you should post messages to the web worker and
-    // receive messages from the web worker.
-
-    length = imageData.data.length / 4;
-    for (i = j = 0, ref = length; 0 <= ref ? j <= ref : j >= ref; i = 0 <= ref ? ++j : --j) {
-      r = imageData.data[i * 4 + 0];
-      g = imageData.data[i * 4 + 1];
-      b = imageData.data[i * 4 + 2];
-      a = imageData.data[i * 4 + 3];
-      pixel = manipulate(type, r, g, b, a);
-      imageData.data[i * 4 + 0] = pixel[0];
-      imageData.data[i * 4 + 1] = pixel[1];
-      imageData.data[i * 4 + 2] = pixel[2];
-      imageData.data[i * 4 + 3] = pixel[3];
+    ImageManipulationWorker.postMessage({imageData, type})
+    ImageManipulationWorker.onmessage = e => {
+      toggleButtonsAbledness();
+      ctx.putImageData(e.data, 0, 0);
     }
-    toggleButtonsAbledness();
-    return ctx.putImageData(imageData, 0, 0);
   };
 
   function revertImage() {
